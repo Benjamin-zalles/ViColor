e14ed5d2682fedcc96f4bf4209312d7be7fddc55
Benjamin <benjaminzalles@gmail.com>
2022-12-08 13:48:22 -0400

    style contrast ratio page

--- src/pages/ContrastRatio.jsx
+++ src/pages/ContrastRatio.jsx
@@ -60,7 +60,7 @@ export function calculateColorBlack0rWhite(co) {
 }
 
 export default function ContrastRatio() {
-  const [color, setColor] = useState("#2D2D2D");
+  const [color, setColor] = useState("#00EFD8"); //#2D2D2D
   const [color2, setColor2] = useState("#14474B");
   const [texto, setTexto] = useState("Escribe tu propio título aquí");
   // Accediendo al body y pathname de la url para cambiar el background del body
@@ -92,21 +92,27 @@ export default function ContrastRatio() {
     }
   };
 
+  const borderInputColor = calculateColorBlack0rWhite(color) === 'colorBlack' ? 'boderBlack' : 'borderWhite'
+
+
+
   return (
     <>
       <div className="contrastRatio-content-one">
         <div className="contrastRatio-text-and-forms">
-          <div>
-            <h2>
-              <input
-                value={texto}
-                onChange={(e) => setTexto(e.target.value)}
-                className="title-input"
-                style={{
-                  color: color2,
-                }}
-              />
-            </h2>
+          <div className="text-c-r">
+            <textarea
+              name="textarea"
+              cols="30"
+              rows="10"
+              className="textarea"
+              maxLength="35"
+              value={texto}
+              onChange={(e) => setTexto(e.target.value)}
+              style={{
+                color: color2,
+              }}
+            ></textarea>
             <p style={{ color: color2 }}>
               El color es la impresión producida por un tono de luz en los
               órganos visuales, o más exactamente, es una percepción visual que
@@ -114,20 +120,20 @@ export default function ContrastRatio() {
               interpretar las señales nerviosas
             </p>
           </div>
-          <div>
+          <div className="container-draw">
             <div className="contrastRatio-forms">
-              <FaCircle size="50px" color={color2} />
-              <FaStop size="50px" color={color2} />
-              <FaStar size="50px" color={color2} />
-              <FaSpinner size="50px" color={color2} />
-              <FaBacon size="50px" color={color2} />
+              <FaCircle size="60px" color={color2} />
+              <FaStop size="60px" color={color2} />
+              <FaStar size="60px" color={color2} />
+              <FaSpinner size="60px" color={color2} />
+              <FaBacon size="60px" color={color2} />
             </div>
-            <div className="constrastRatio-icons">
-              <FaApple size="50px" color={color2} />
-              <FaHome size="50px" color={color2} />
-              <FaHamburger size="50px" color={color2} />
-              <FaSnapchatGhost size="50px" color={color2} />
-              <FaShoppingCart size="50px" color={color2} />
+            <div className="contrastRatio-icons">
+              <FaApple size="60px" color={color2} />
+              <FaHome size="60px" color={color2} />
+              <FaHamburger size="60px" color={color2} />
+              <FaSnapchatGhost size="60px" color={color2} />
+              <FaShoppingCart size="60px" color={color2} />
             </div>
           </div>
         </div>
@@ -135,28 +141,37 @@ export default function ContrastRatio() {
         <div className="contrastRatio-color-picker">
           {/* selectores de color para text y fondo */}
           <div className="color-picker">
-            <div className="picker-fondo">
-              <h1 className={calculateColorBlack0rWhite(color)}>Fondo</h1>
-              <HexColorPicker color={color} onChange={setColor} />
-              <input onChange={(e) => setColor(e.target.value)} value={color} />
+            <div className="picker-fondo picker-r">
+              <div>
+                <h2 className={calculateColorBlack0rWhite(color)}>Fondo</h2>
+                <HexColorPicker color={color} onChange={setColor}/>
+              </div>
+              <input
+                onChange={(e) => setColor(e.target.value)}
+                value={color}
+                className={`input-c ${calculateColorBlack0rWhite(color)} ${borderInputColor}`}
+              />
             </div>
-            <div className="picker-texto">
-              <h1 className={calculateColorBlack0rWhite(color)}>Texto</h1>
-              <HexColorPicker color={color2} onChange={setColor2} />
+            <div className="picker-texto picker-r">
+              <div>
+                <h2 className={calculateColorBlack0rWhite(color)}>Texto</h2>
+                <HexColorPicker color={color2} onChange={setColor2} />
+              </div>
               <input
                 onChange={(e) => setColor2(e.target.value)}
                 value={color2}
+                className={`input-c ${calculateColorBlack0rWhite(color)} ${borderInputColor}`}
               />
             </div>
           </div>
 
           {/* resultados */}
-          <div>
+          <div className="t-c-r">
             <table
-              className={calculateColorBlack0rWhite(color)}
+              className={`tabla-contrast ${calculateColorBlack0rWhite(color)}`}
               border="1"
               style={{
-                border: "3px solid",
+                border: "1px solid",
                 borderCollapse: "collapse",
                 overflow: "hidden",
               }}
@@ -165,21 +180,22 @@ export default function ContrastRatio() {
                 <tr className={calculateColorBlack0rWhite(color)}>
                   <th
                     style={{
-                      border: "3px solid",
+                      border: "1px solid",
+                      height: "50px"
                     }}
                   >
                     Título
                   </th>
                   <th
                     style={{
-                      border: "3px solid",
+                      border: "1px solid",
                     }}
                   >
                     Texto Normal
                   </th>
                   <th
                     style={{
-                      border: "3px solid",
+                      border: "1px solid",
                     }}
                   >
                     Formas
@@ -191,28 +207,38 @@ export default function ContrastRatio() {
                   <td
                     className={calculateColorBlack0rWhite(color)}
                     style={{
-                      border: "3px solid",
+                      border: "1px solid",
+                      textAlign: "center",
+                      width: "30%",
                     }}
                   >
                     <div>
-                      <p>{calculateContrast(color, color2)}</p>
+                      <p className="calculo-num">{calculateContrast(color, color2)}</p>
                       <p>{levelAALarge(checkColor)}</p>
                     </div>
                   </td>
                   <td
                     className={calculateColorBlack0rWhite(color)}
                     style={{
-                      border: "3px solid",
+                      border: "1px solid",
+                      textAlign: "center",
+                      width: "30%"
                     }}
                   >
                     <div>
-                      <p>{calculateContrast(color, color2)}</p>
+                      <p className="calculo-num">{calculateContrast(color, color2)}</p>
                       <p>{levelAANormal(checkColor)}</p>
                     </div>
                   </td>
-                  <td className={calculateColorBlack0rWhite(color)}>
+                  <td className={calculateColorBlack0rWhite(color)}
+                    style={{
+                      border: "1px solid",
+                      textAlign: "center",
+                      width: "30%",
+                    }}
+                  >
                     <div>
-                      <p>{calculateContrast(color, color2)}</p>
+                      <p className="calculo-num">{calculateContrast(color, color2)}</p>
                       <p>{levelAANormal(checkColor)}</p>
                     </div>
                   </td>
@@ -222,13 +248,15 @@ export default function ContrastRatio() {
           </div>
         </div>
       </div>
-
+      {/* SEGUNDO CONTENIDO */}
       <div className="contrastRatio-content-two">
         <div className="title-content-two-contrast">
-          <h3>Criterio de conformindad realacionado con la WCAG</h3>
+          <h3 className={calculateColorBlack0rWhite(color)}>
+            Criterio de conformindad realacionado con la WCAG
+          </h3>
         </div>
         <div className="explicacion-contrast">
-          <div>
+          <div className={calculateColorBlack0rWhite(color)}>
             <h3>Nivel AA</h3>
             <p>
               El contraste de color entre el fondo y el contenido del primer
@@ -244,11 +272,20 @@ export default function ContrastRatio() {
               más grande se define como de al menos 18pt
             </p>
           </div>
-          <div>
+          <div className={calculateColorBlack0rWhite(color)}>
             <h3>Nivel AAA</h3>
-            <p>El contraste de color se basa en el criterio 1.4.3 que es la versión mejorada del nivel AA.</p>
-            <p>El texto y el fondo deben tener una relación de contraste de al menos 7:1 para texto normal (17pt e inferior)</p>
-            <p>Para texto grande (18pt y superior) deben tener una relación de contraste de al menos 4.5:1.</p>
+            <p>
+              El contraste de color se basa en el criterio 1.4.3 que es la
+              versión mejorada del nivel AA.
+            </p>
+            <p>
+              El texto y el fondo deben tener una relación de contraste de al
+              menos 7:1 para texto normal (17pt e inferior)
+            </p>
+            <p>
+              Para texto grande (18pt y superior) deben tener una relación de
+              contraste de al menos 4.5:1.
+            </p>
           </div>
         </div>
       </div>
